9089d04549379f38f5e5fb9672e1d8a0304375be
sinne0r <ernestrsage@gmail.com>
2023-11-08 18:48:07 +0300

    last

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,9 +1,12 @@
+<!-- MainPage.vue -->
+
 <template>
   <div>
     <Navbar />
     <h1>Торговля</h1>
     <p v-if="brokerData">Привет, {{ brokerData.name }}!</p>
     <p v-else>Loading...</p>
+    <!-- Основной функционал торговли и отображение информации -->
   </div>
 </template>
 
